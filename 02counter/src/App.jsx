5dcdc4756870c96a22bf306d42d02cb4ec8fdb05
Shresth-11 <jaisshresth143@gmail.com>
2026-02-16 13:25:44 +0530

    counter project

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,32 +1,42 @@
-import "./App.css";
-import { useState } from "react";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  let [counter, setCounter] = useState(15);
 
-  // let counter = 5;
+  const [counter, setCounter]  = useState(15)
+
+  //let counter = 15
 
   const addValue = () => {
-    counter = counter + 1;
-    setCounter(counter);
-    console.log("clicked", counter);
-  };
+    //counter = counter + 1
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1 )
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    
+  }
 
   const removeValue = () => {
-    setCounter(counter - 1);
-  };
-
+    setCounter(counter - 1)
+  }
+  
   return (
     <>
       <h1>Chai aur react</h1>
       <h2>Counter value: {counter}</h2>
 
-      <button onClick={addValue}>Add value</button>
+      <button
+      onClick={addValue}
+      >Add value {counter}</button> 
       <br />
-      <br />
-      <button onClick={removeValue}>remove value</button>
+      <button
+      onClick={removeValue}
+      >remove value {counter}</button>
+      <p>footer: {counter}</p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
