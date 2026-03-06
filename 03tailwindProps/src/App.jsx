52eaf2222f21d6d44291e63b424ec12a260d452a
Shresth-11 <jaisshresth143@gmail.com>
2026-03-06 15:04:19 +0530

    app file

--- 03tailwindProps/src/App.jsx
+++ 03tailwindProps/src/App.jsx
@@ -22,6 +22,34 @@ function App() {
 export default App
 
 
+
+// REACT FIBRE    ***********************************
+
+//React Fiber is an ongoing reimplementation of React's core algorithm.
+//  It is the culmination of over two years of research by the React team.
+// The goal of React Fiber is to increase its suitability for areas like animation, 
+// layout, and gestures. Its headline feature is incremental rendering: the ability to 
+// split rendering work into chunks and spread it out over multiple frames.
+
+// Other key features include the ability to pause, abort, or reuse work as
+//  new updates come in; the ability to assign priority to different types of updates;
+//  and new concurrency primitives.
+
+
+
+
+// reconciliation ----->>>
+// The algorithm React uses to diff one tree with another to determine which parts need to be changed.
+
+
+// Diffing of lists is performed using keys. Keys should be "stable, predictable, and unique." Since fibre which is introduced , 
+// increases performance using keys and if keys  are not used then the performnce degrades too much.
+
+
+
+
+// React createRoot create a virtual DOM which updates the ui where it is changed or updated
+
 // In react there is still a concept of virtual dom and it updated by use of fibre algorithm
 
 // Reconciliation is the algorithm behind what is probably understood  as the "Virtual DOM"
