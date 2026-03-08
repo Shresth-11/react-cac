ef6bec72573284c9810317de94c7f4ac9a9f7ad7
Shresth-11 <jaisshresth143@gmail.com>
2026-03-08 15:53:18 +0530

    useEffect, useCallback, useRef, useState

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,112 +1,122 @@
-import { useState, useCallback, useEffect, useRef } from 'react'
-
-
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
-  const [length, setLength] = useState(8)
+  const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
-  const [charAllowed, setCharAllowed] = useState(false)
-  const [password, setPassword] = useState("")
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
 
   //useRef hook
-  const passwordRef = useRef(null)
+  // kisi bhi cheez ka reference le lena hota h tb useRef hook kaam me aata hai
+
+  const passwordRef = useRef(null);
 
 
-// useCallback is a React Hook that lets you cache a function definition between re-renders.
 
+
+
+  // useCallback is a React Hook that lets you cache a function definition between re-renders.
 
   const passwordGenerator = useCallback(() => {
-    let pass = ""
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    if (numberAllowed) str += "0123456789"
-    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`";
 
     for (let i = 1; i <= length; i++) {
-      let char = Math.floor(Math.random() * str.length + 1)
-      pass += str.charAt(char)  
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
     }
 
-    setPassword(pass)
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
 
-  }, [length, numberAllowed, charAllowed, setPassword])
+  // we have used setPassword to memoize , this is a concept of memoization,
+  // useCallback yaad rkh leta h function ko or useEffect wapis run kr deta hai based on dependencies agr usme change hua ho kuch to
+  // useCallback is resposible for memoize the function, optimize krta h cache me rkhta hai
+  // setPassword bss optimize krne k liye rkha h dependencies me memoize or cache me rkhne k liye setPassword ko daala h dependency me simple
 
 
 
-                   
+
+
+
 
   const copyPasswordToClipboard = useCallback(() => {
     passwordRef.current?.select();
     passwordRef.current?.setSelectionRange(0, 999);
-    window.navigator.clipboard.writeText(password)
-  }, [password])
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
 
 
 
 
+
+
+  // useEffect renders or reloads the function automatically based on dependencies on page reload
   useEffect(() => {
-    passwordGenerator()
-  }, [length, numberAllowed, charAllowed, passwordGenerator])
-
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
 
   return (
-    
     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
-      <h1 className='text-white text-center my-3'>Password generator</h1>
-    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+      <h1 className="text-white text-center my-3">Password generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
         <input
-            type="text"
-            value={password}
-            className="outline-none w-full py-1 px-3 bg-white"
-            placeholder="Password"
-            readOnly
-            ref={passwordRef}
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3 bg-white"
+          placeholder="Password"
+          readOnly
+          ref={passwordRef}    // useRef
         />
         <button
-        onClick={copyPasswordToClipboard}
-        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
-        >copy</button>
-        
-    </div>
+          onClick={copyPasswordToClipboard}
+          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+        >
+          copy
+        </button>
+      </div>
 
-
-    <div className='flex text-sm gap-x-2'>
-      <div className='flex items-center gap-x-1'>
-        <input 
-        type="range"
-        min={6}
-        max={100}
-        value={length}
-         className='cursor-pointer'
-         onChange={(e) => {setLength(e.target.value)}}
+      <div className="flex text-sm gap-x-2">
+        <div className="flex items-center gap-x-1">
+          <input
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => {
+              setLength(e.target.value);
+            }}
           />
           <label>Length: {length}</label>
-      </div>
-      <div className="flex items-center gap-x-1">
-      <input
-          type="checkbox"
-          defaultChecked={numberAllowed}
-          id="numberInput"
-          onChange={() => {
-              setNumberAllowed((prev) => !prev);
-          }}
-      />
-      <label htmlFor="numberInput">Numbers</label>
-      </div>
-      <div className="flex items-center gap-x-1">
+        </div>
+        <div className="flex items-center gap-x-1">
           <input
-              type="checkbox"
-              defaultChecked={charAllowed}
-              id="characterInput"
-              onChange={() => {
-                  setCharAllowed((prev) => !prev )
-              }}
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={charAllowed}
+            id="characterInput"
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }}
           />
           <label htmlFor="characterInput">Characters</label>
+        </div>
       </div>
     </div>
-</div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
