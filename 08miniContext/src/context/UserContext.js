158dae617b0b99010c0a6cdae02b8753e5d9967c
Shresth-11 <jaisshresth143@gmail.com>
2026-03-28 17:03:01 +0530

    created user context

---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserContext = React.createContext()
+
+
+// har ek context provider hota h   
+// context bnta h jo globally accessible hota h ,    
+//  hume faltu mai hrr ek component me data failane ki jrurt ni, direct globally access     
+//  kr skte hain with help of context provider    
+
+
+export default UserContext;
